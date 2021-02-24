30c454c678d5f3dc5c04118f5e1bfb6f58e2b2f4
Brian Altamirano <altbrian@csu.fullerton.edu>
2021-02-23 22:39:18 -0800

    Another Commit Test
    ?

---
+++ Assets/Scripts/SpeedDotMenuScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedDotMenuScript : MonoBehaviour
+{
+    public void OnButtonClickTest()
+    {
+        Debug.Log("Button Pressed");
+    }
+}
