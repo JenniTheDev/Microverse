1795ff0bf149d7082ebefea45e0fc03441d889b5
Jenni <justlittleoldmejenn@gmail.com>
2021-03-03 18:56:29 -0800

    More sample scripts

---
+++ Assets/Scripts/GameEventFloat.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+namespace GameEvents.Events {
+    [CreateAssetMenu(fileName = "New Game Event (float)", menuName = "Game Events/New Game Event (float)")]
+    public class GameEventFloat : BaseGameEvent<float> { }
+}
