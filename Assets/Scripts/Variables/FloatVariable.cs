2e9f90c2260106778f6bd2b5da1884005984b138
Jenni <justlittleoldmejenn@gmail.com>
2021-03-09 23:43:47 -0800

    Volume slider works, saves values
    Starting on Audio Mixer

--- Assets/Scripts/Variables/FloatVariable.cs
+++ Assets/Scripts/Variables/FloatVariable.cs
@@ -1,12 +1,14 @@
+// Jenni
+// Modification of RoboRyanTron's Unite 2017's
+// game architecture with scriptable objects
 using UnityEngine;
 
 namespace Variables {
     [CreateAssetMenu(fileName = "New Game Variable (Float)", menuName = "Game Variable/New Game Variable (float)")]
     public class FloatVariable : ScriptableObject {
+        public float Value;
 
-        private float Value;
-
-        public void SetValue (float value) {
+        public void SetValue(float value) {
             Value = value;
         }
 
