3a435b01d1be931d53b68c8a55f0bddca270519d
Jenni <justlittleoldmejenn@gmail.com>
2021-03-13 21:52:12 -0700

    Buttons created and placed

--- Assets/Scripts/JenniSays/JSGameLogic.cs
+++ Assets/Scripts/JenniSays/JSGameLogic.cs
@@ -4,34 +4,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class JSGameLogic : MonoBehaviour {
-    // I don't think I need this
-    // [SerializeField] private GameObject gameButtonPrefab;
-    [SerializeField] private List<ButtonSetting> buttonSettings;
-    private Transform gameFieldLayout;
 
-    [SerializeField]private List<GameObject> gameButtons;
-    private int numberOfClicks = 0;
-
-    [SerializeField] private List<int> jenniSaid;
-    [SerializeField] private List<int> playerSaid;
-
-    System.Random rg;
-
+    // List of buttons in the game 
+    [SerializeField] private List<GameObject> gameButtons;
+    // list that is empty, but filled as the computer selects a button
+    [SerializeField] private List<GameObject> orderToMatch;
+    // list of buttons pressed
+    [SerializeField] private List<GameObject> playerChoices;
+    // Can't push buttons while the game is playing
     private bool inputEnabled = false;
-    private bool gameOver = false;
-
+    private bool gameFailed = false;
+    // inrement after every successful pattern picked
+    private int gameLevel = 0;
     
-    private void Start() {
-        InitalizeGameBoard();
-    }
+    
 
-    private void InitalizeGameBoard() {
-        gameButtons = new List<GameObject>();
+    void Start() {
         
-        
-
     }
 
     private void Update() {
+        
     }
+
+    private void ResetButtons() {
+
+    }
+
+    private void PickOrderToSay() {
+
+    }
+
+    private void PlayerGuesses() {
+        inputEnabled = true;
+
+    }
+
+    private void CheckOrder() {
+
+    }
+
+
+
 }
