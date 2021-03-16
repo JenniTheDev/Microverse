57cec0bdf763170844d491b2f036c7782a1d41aa
Vast <Vastlee@gmail.com>
2021-03-15 20:40:03 -0700

    Animation Fix

--- Assets/Scripts/JenniSays/JSGameLogic.cs
+++ Assets/Scripts/JenniSays/JSGameLogic.cs
@@ -6,9 +6,19 @@ using UnityEngine.InputSystem;
 
 namespace JenniSays {
     public class JSGameLogic : MonoBehaviour {
+        enum GameMode {
+            NONE, PlayingBack, Receiving
+        }
+
+        [SerializeField]
+        private int intGameLevel;
+
+        [SerializeField]
+        private float speedIncrease = 0.0f;
 
         // List of buttons in the game
-        [SerializeField] private List<JSButton> gameButtons;
+        [SerializeField]
+        private List<JSButton> gameButtons;
 
         // list that is empty, but filled as the computer selects a button
         // maybe these two lists can just be ints?
@@ -19,11 +29,8 @@ namespace JenniSays {
 
         // inrement after every successful pattern picked
         private int gameLevel = 1;
-        [SerializeField] private int intGameLevel;
-        
-        [SerializeField] private float speedIncrease = 0.0f;
-        [SerializeField] private Animator buttonAnimation;
-        [SerializeField] private InputAction input;
+
+        private GameMode currentMode = GameMode.NONE;
 
         private void Start() {
             ResetButtons();
@@ -37,28 +44,28 @@ namespace JenniSays {
         private void ResetButtons() {
             orderToMatch = new List<JSButton>();
             gameLevel = 1;
-            
+
         }
 
         private void StartGame() {
             StartCoroutine(PlayButtonSequence(orderToMatch, speedIncrease));
         }
 
-      
+
         private void AddRandomButton() {
-           int buttonToAdd = UnityEngine.Random.Range(0, gameButtons.Count);
+            int buttonToAdd = UnityEngine.Random.Range(0, gameButtons.Count);
             orderToMatch.Add(gameButtons[buttonToAdd]);
         }
 
         private void AddRandomButtons(int numToAdd) {
-            for (int i = 0; i < numToAdd; i++) {
+            for(int i = 0; i < numToAdd; i++) {
                 AddRandomButton();
             }
         }
 
         private void IsPlayerCorrect() {
             inputEnabled = true;
-            for (int i = 0; i < gameLevel; i++) {
+            for(int i = 0; i < gameLevel; i++) {
                 // player input collected button click listener? Event passing parameter?
                 // player input != to orderToMatch[i];
                 //  return false ;
@@ -66,27 +73,29 @@ namespace JenniSays {
         }
 
         private void OnGameButtonClick() {
-            if (!inputEnabled) {
+            if(!inputEnabled) {
                 return;
             }
         }
 
         private IEnumerator PlayButtonSequence(List<JSButton> buttons, float pauseTime) {
-            
-         
+            currentMode = GameMode.PlayingBack;
             WaitForSeconds waitTime = new WaitForSeconds(pauseTime);
-            foreach (var button in buttons) {
+            foreach(var button in buttons) {
                 ActivateButton(button);
                 yield return waitTime;
             }
-             
+            currentMode = GameMode.NONE;
         }
 
         public void ActivateButton(JSButton selectedButton) {
             selectedButton.ButtonAnimation.Play();
-           // Debug.Log($"Button {selectedButton.gameObject.name}");
+            if(this.currentMode == GameMode.Receiving) {
+
+            }
+            // Debug.Log($"Button {selectedButton.gameObject.name}");
         }
 
-        
+
     }
 }
