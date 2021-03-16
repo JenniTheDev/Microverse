f1a8f274f763eb03816d8d7f6779de96ed6ca234
Jenni <justlittleoldmejenn@gmail.com>
2021-03-15 17:05:24 -0700

    Removing unneeded code

--- Assets/Scripts/JenniSays/JSGameLogic.cs
+++ Assets/Scripts/JenniSays/JSGameLogic.cs
@@ -14,27 +14,18 @@ namespace JenniSays {
         // maybe these two lists can just be ints?
         [SerializeField] private List<GameObject> orderToMatch;
 
-        // list of buttons pressed
-        [SerializeField] private List<GameObject> playerChoices;
-
         // Can't push buttons while the game is playing
         private bool inputEnabled = false;
-
-        private bool gameFailed = false;
 
         // inrement after every successful pattern picked
         private int gameLevel = 1;
 
-        [SerializeField] private int maxGameLevel = 50;
-
-        [SerializeField] private float increasePerLevel = 0.0f;
+        [SerializeField] private float speedIncrease = 0.0f;
         [SerializeField] private Animator buttonAnimation;
         [SerializeField] private InputAction input;
 
         private void Start() {
             ResetButtons();
-            // buttonAnimation.Play("ActiveButton");
-            StartGame();
         }
 
         private void Update() {
@@ -42,22 +33,17 @@ namespace JenniSays {
 
         private void ResetButtons() {
             orderToMatch = new List<GameObject>();
-            playerChoices = new List<GameObject>();
-            gameFailed = false;
             gameLevel = 1;
-            PickOrderToSay();
+            OrderToSay();
         }
 
         private void StartGame() {
-            StartCoroutine(PlayButtonSequence(orderToMatch, increasePerLevel));
+            StartCoroutine(PlayButtonSequence(orderToMatch, speedIncrease));
         }
 
-        private void PickOrderToSay() {
-            int buttonToAdd;
-            for (int i = 0; i < maxGameLevel; i++) {
-                buttonToAdd = UnityEngine.Random.Range(0, gameButtons.Count - 1);
-                orderToMatch.Add(gameButtons[buttonToAdd]);
-            }
+        private void OrderToSay() {
+            int buttonToAdd = UnityEngine.Random.Range(0, gameButtons.Count - 1);
+            orderToMatch.Add(gameButtons[buttonToAdd]);
         }
 
         private void IsPlayerCorrect() {
@@ -74,30 +60,6 @@ namespace JenniSays {
                 return;
             }
         }
-
-        private void AddPlayerGuess() {
-            GameObject buttonPushed = GetComponent<GameObject>();
-        }
-
-        //private bool CheckOrder() {
-        //    bool result = false;
-        //    for (int i = 0; i < gameLevel; i++) {
-        //        result = orderToMatch[i] == playerChoices[i];
-        //        if (result) { continue; } else { return false; }
-        //    }
-
-        //    return result;
-        //}
-
-        //private bool DidWeWinYet() {
-        //    if (CheckOrder() == false) {
-        //        return false;
-        //        //broadcast game over
-        //    } else {
-        //        // continue on with Jenni picking
-        //        return true;
-        //    }
-        //}
 
         private IEnumerator PlayButtonSequence(List<GameObject> buttons, float pauseTime) {
             inputEnabled = false;
