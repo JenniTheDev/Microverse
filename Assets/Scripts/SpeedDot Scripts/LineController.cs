5ecc1669dc0a9a7c0b6b6d8f6289ce8aa784cd24
Jenni <justlittleoldmejenn@gmail.com>
2021-03-27 20:56:34 -0700

    Out Of Bounds and Missing Reference

--- Assets/Scripts/SpeedDot Scripts/LineController.cs
+++ Assets/Scripts/SpeedDot Scripts/LineController.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace SpeedDot {
     public class LineController : MonoBehaviour {
-        private List<Transform> pointsToConnect;
+        [SerializeField]private List<Transform> pointsToConnect;
         [SerializeField] private LineRenderer lineBetweenPoints;
 
         private void Start() {
