92d055a746a33bb04a75c51b9a231fc234d5a3ca
Jenni <justlittleoldmejenn@gmail.com>
2021-03-27 20:31:19 -0700

    Fixed out of bounds

--- Assets/Scripts/SpeedDot Scripts/LineController.cs
+++ Assets/Scripts/SpeedDot Scripts/LineController.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace SpeedDot {
     public class LineController : MonoBehaviour {
-        [SerializeField] private List<Transform> pointsToConnect;
+        private List<Transform> pointsToConnect;
         private LineRenderer lineBetweenPoints;
 
         private void Start() {
@@ -21,7 +21,13 @@ namespace SpeedDot {
         public void GetButtonLocation(JSButton selectedButton) {
            pointsToConnect.Add(selectedButton.gameObject.transform);
         }
-        // ON click draw line? 
+
+        public void SetUpLine(List<Transform> points) {
+            lineBetweenPoints.positionCount = pointsToConnect.Count;
+            this.pointsToConnect = points;
+        }
+        
+
         private void DrawLines() {
             for (int i = 0; i < pointsToConnect.Count; i++) {
                 lineBetweenPoints.SetPosition(i, pointsToConnect[i].position);
