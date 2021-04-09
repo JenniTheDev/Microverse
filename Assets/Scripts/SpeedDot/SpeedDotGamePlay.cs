94ce4202f7c6c9e79b290435dc64e8cefa4bb85d
Jenni <justlittleoldmejenn@gmail.com>
2021-04-08 19:17:02 -0700

    Stopping to try reusing JenniSays

--- Assets/Scripts/SpeedDot/SpeedDotGamePlay.cs
+++ Assets/Scripts/SpeedDot/SpeedDotGamePlay.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,10 +7,11 @@ namespace SpeedDot {
         // event for level change
         [SerializeField]
         private List<SpeedDot> allDots;
-        private List<SpeedDot> clickedDots;
+
+        private List<SpeedDot> dotsToMatch;
 
         [SerializeField]
-        private int gameLevel = 1;
+        private int gameLevel = 0;
 
         [SerializeField]
         private int initalLevel;
@@ -19,36 +19,52 @@ namespace SpeedDot {
         [SerializeField]
         private int resetLevel;
 
-        // if all dots clicked are false, next level
-        // time up game over
+        private int randomIndex;
 
         private void Start() {
             ResetGame();
+            NextLevel();
+        }
+
+        private void Update() {
+             
+        }
+
+        private void PlayGame() {
+            SetUpDots();
+            
         }
 
         private void ResetGame() {
             gameLevel = initalLevel;
-
         }
+
+
 
         private void AddDot(SpeedDot clickedButton) {
-            // clickedButton.ButtonAnimation.Play();
-            clickedDots.Add(clickedButton);
-
+            //clickedButton.ButtonAnimation.Play();
+           // clickedDots.Add(clickedButton);
         }
 
-        private void DotActivator() {
-    
+        private void SetUpDots() {
+            for (int i = 0; i < gameLevel + 1; i++) {
+                randomIndex = Random.Range(0, allDots.Count);
+                if (!allDots[randomIndex].gameObject.activeSelf) {
+                    allDots[randomIndex].gameObject.SetActive(true);
+                } else {
+                    i--;
+                }
+            }
         }
+
         private void NextLevel() {
-            foreach (var dot in clickedDots) {
-                // dot.SetActive(false);
+            foreach (var dot in dotsToMatch) {
+                dot.gameObject.SetActive(false);
             }
             gameLevel++;
         }
-        private void CheckList() {
 
-            
+        private void CheckList() {
         }
     }
 }
