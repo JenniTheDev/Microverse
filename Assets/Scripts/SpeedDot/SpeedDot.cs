72528918048bd4a117cedf9f49105d9928fa0fdf
Jenni <justlittleoldmejenn@gmail.com>
2021-04-08 22:16:26 -0700

    Dots not being added to list

--- Assets/Scripts/SpeedDot/SpeedDot.cs
+++ Assets/Scripts/SpeedDot/SpeedDot.cs
@@ -5,8 +5,10 @@ namespace SpeedDot {
     public class SpeedDot : MonoBehaviour {
         [SerializeField]
         private SOEvents.Events.GameEventSpeedDot onClick;
+        [SerializeField] private Animation dotAnimation;
 
         public bool HasBeenClicked { get; set; } = false;
+        public Animation DotAnimation { get => dotAnimation; }
 
         #region MonoBehaviour
         private void OnMouseDown() {
---
+++ Assets/Scripts/SpeedDot/SpeedDotGameLogic.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpeedDot {
+    public class SpeedDotGameLogic : MonoBehaviour {
+
+        private enum GameMode {
+            NONE, PlayingBack, Receiving
+        }
+
+        [SerializeField]
+        private int gameLevel = 2;
+
+        [SerializeField]
+        private int initialLevel;
+
+        [SerializeField]
+        private int resetLevel;
+
+        [SerializeField]
+        private float speedIncrease = 0.0f;
+
+        [SerializeField]
+        private List<SpeedDot> gameDots;
+
+        private List<SpeedDot> orderToMatch;
+
+        private int currentIndex;
+        private float pauseBetweenLevels = 2.0f;
+
+        private GameMode currentMode = GameMode.NONE;
+
+        private void Start() {
+            ResetGame();
+            AddRandomDots(gameLevel);
+            StartCoroutine(PlayButtonSequence(orderToMatch, speedIncrease));
+        }
+
+        private void Update() {
+        }
+
+        private void ResetGame() {
+            currentMode = GameMode.PlayingBack;
+            orderToMatch = new List<SpeedDot>();
+            gameLevel = initialLevel;
+            currentIndex = 0;
+        }
+
+        private void StartGame() {
+            StartCoroutine(PlayButtonSequence(orderToMatch, speedIncrease));
+        }
+
+        private void AddRandomDot() {
+            int buttonToAdd = UnityEngine.Random.Range(0, gameDots.Count);
+            orderToMatch.Add(gameDots[buttonToAdd]);
+        }
+
+        private void AddRandomDots(int numToAdd) {
+            for (int i = 0; i < numToAdd; i++) {
+                AddRandomDot();
+            }
+        }
+
+        private void GameOver() {
+            // Game Manager Broadcast Game Over
+            ResetGame();
+            Debug.Log("Start Over");
+        }
+
+        private IEnumerator PlayButtonSequence(List<SpeedDot> dots, float pauseTime) {
+            currentMode = GameMode.PlayingBack;
+            WaitForSeconds waitTime = new WaitForSeconds(pauseTime);
+            yield return new WaitForSeconds(pauseBetweenLevels);
+            foreach (var dot in dots) {
+                ActivateDot(dot);
+                yield return waitTime;
+            }
+            currentMode = GameMode.Receiving;
+        }
+
+        public void ActivateDot(SpeedDot selectedDot) {
+            selectedDot.DotAnimation.Play();
+            if (this.currentMode == GameMode.Receiving && currentIndex < orderToMatch.Count) {
+                if (selectedDot == orderToMatch[currentIndex]) {
+                    Debug.Log("Match");
+                    currentIndex++;
+                } else {
+                    GameOver();
+                }
+            }
+            if (currentIndex == orderToMatch.Count && currentIndex != 0) {
+                NextLevel();
+            }
+        }
+
+        private void NextLevel() {
+            Debug.Log("Next Level");
+            currentIndex = 0;
+            gameLevel++;
+            currentMode = GameMode.PlayingBack;
+            AddRandomDot();
+            StartGame();
+        }
+    }
+}
