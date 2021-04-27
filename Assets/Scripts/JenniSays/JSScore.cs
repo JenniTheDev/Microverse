37d02d9519ee89276f3354eade8092d35a151054
Jenni <justlittleoldmejenn@gmail.com>
2021-04-27 13:29:17 -0700

    Removed unneeded variable

--- Assets/Scripts/JenniSays/JSScore.cs
+++ Assets/Scripts/JenniSays/JSScore.cs
@@ -1,16 +1,13 @@
 // Jenni
+using TMPro;
 using UnityEngine;
 using Variables;
-using TMPro;
-using UnityEngine.UI;
 
 public class JSScore : MonoBehaviour {
     [SerializeField] private IntVariable score;
     [SerializeField] private TMP_Text scoreText;
-    private int scoreTemp;
 
     private void Update() {
-        scoreTemp = score.GetValue();
-        scoreText.text = "Level " + scoreTemp.ToString();
+        scoreText.text = "Level " + score.GetValue().ToString();
     }
 }
