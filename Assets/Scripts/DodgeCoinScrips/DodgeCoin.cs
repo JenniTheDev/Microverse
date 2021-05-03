26c268416e71a6fcd70c3615dd522e48f4064ef1
Jenni <justlittleoldmejenn@gmail.com>
2021-05-03 15:27:26 -0700

    Pausing to add in gameManager

--- Assets/Scripts/DodgeCoinScrips/DodgeCoin.cs
+++ Assets/Scripts/DodgeCoinScrips/DodgeCoin.cs
@@ -7,9 +7,10 @@ public class DodgeCoin : MonoBehaviour {
     [SerializeField] private float maxStartAngle;
     [SerializeField] private float maxMagnitude;
     [SerializeField] private float minMagnitude;
+    //[SerializeField] private GameManager gameManager;
 
     private LayerMask wallLayer;
-    private LayerMask player;
+    private LayerMask playerLayer;
     private Rigidbody2D rBody;
 
     #region Properties
@@ -21,13 +22,32 @@ public class DodgeCoin : MonoBehaviour {
     private void Awake() {
         rBody = GetComponent<Rigidbody2D>();
         wallLayer = LayerMask.NameToLayer("Wall");
-        player = LayerMask.NameToLayer("Player");
+        playerLayer = LayerMask.NameToLayer("Player");
     } 
 
-    public void LaunchCoin() {
+    public void StartCoin() {
+        SpawnCoin();
+        LaunchCoin();
         
     }
 
+    private void SpawnCoin() {
+        int startPosX = (UnityEngine.Random.Range(-600, 600));
+        int startPosY = (UnityEngine.Random.Range(-500, 500));
+        Vector2 dir = new Vector2();
+       // transform.position = Vector2(startPosX, startPosY);
+       // need to make a random spawn position
+    }
+
+    private void LaunchCoin() {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.layer == playerLayer) {
+            // game manager call game over
+        }
+    }
 
     #endregion
 
