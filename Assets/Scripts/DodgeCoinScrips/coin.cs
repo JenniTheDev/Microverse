2b7c693b0d5c6f74a0327b7c5f9bb7e9c8aa6028
Jenni <justlittleoldmejenn@gmail.com>
2021-05-04 10:57:49 -0700

    Coins have velocity when spawned

--- Assets/Scripts/DodgeCoinScrips/coin.cs
+++ Assets/Scripts/DodgeCoinScrips/coin.cs
@@ -20,19 +20,21 @@ public class Coin : MonoBehaviour
         }
         rb = this.GetComponent<Rigidbody2D>();
         rb.velocity = new Vector2(speed * randNum, speed * randNum);
-        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        //screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
 
     }
+
+    
 
     // Update is called once per frame
     void Update()
     {
         
-        if (transform.position.x < screenBounds.x * 2)
-        {
-            //Destroy(this.gameObject);
-            speed = speed * -1;
-        }
+        //if (transform.position.x < screenBounds.x * 2)
+        //{
+        //    //Destroy(this.gameObject);
+        //    speed = speed * -1;
+        //}
         
     }
 
